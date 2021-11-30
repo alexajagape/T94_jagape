b3cd44cd55d91fc5056086977e6cb53a1b6a06d9
alexa gin padernal <89246559+alexajagape@users.noreply.github.com>
2021-11-30 15:17:16 +0800

    Add files via upload

---
+++ main (1).c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+    
+    for (int a = 1; a < 10; a++)
+    {
+        //printf("%d\n", a);
+        for(int b = a; b < 10; b++)
+        {
+            printf("%d", b);
+        }
+    printf("\n");
+    }
+
+    return ;
+}
+    
